7d9605485bb1a68d9689701d52e5a25bcaffc1c1
Dean Hooker <deanhooker91@gmail.com>
2018-04-11 19:38:47 -0600

    shuffle butter

--- src/App.js
+++ src/App.js
@@ -11,6 +11,26 @@ let score = 0;
 let guessedArray = [];
 let hiScore = 0;
 
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+
 class App extends Component {
 
 	state = {
@@ -20,7 +40,7 @@ class App extends Component {
 	};
 
 	newGame = () => {
-		
+
 		guessedArray = [];
 		score = 0;
 		this.setState({ score: 0 });
@@ -29,7 +49,7 @@ class App extends Component {
 	checkHiScore = score => {
 
 		if (score > this.state.hiScore) {
-			this.setState({hiScore: score});
+			this.setState({ hiScore: score });
 		}
 	}
 
@@ -38,34 +58,35 @@ class App extends Component {
 		if (!guessedArray.includes(id)) {
 			guessedArray.push(id);
 			score++;
-			this.setState({score: score});
+			this.setState({ score: score });
 			this.checkHiScore(score);
-		} 
+			shuffle(characters);
+		}
 		else {
 			this.newGame();
 		}
 	}
 
-	
+
 	render() {
-    	return(
+		return (
 			<div>
-    			<Navbar score={this.state.score} hiScore={this.state.hiScore}/>
+				<Navbar score={this.state.score} hiScore={this.state.hiScore} />
 				<Jumbotron />
 				<Container>
-				{characters.map(character => 
-					<CharacterCard 
-					id={character.id}
-					name={character.name}
-					image={character.webImage}
-					guessCharacter={this.guessCharacter}
-					/>
-				)}
+					{characters.map(character =>
+						<CharacterCard
+							id={character.id}
+							name={character.name}
+							image={character.webImage}
+							guessCharacter={this.guessCharacter}
+						/>
+					)}
 				</Container>
 				<Footer />
 			</div>
-    	)
-  	};
+		)
+	};
 };
 
 export default App;
