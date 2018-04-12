52f593bb38ec238411860bd1028d1423ba6ffe53
Dean Hooker <deanhooker91@gmail.com>
2018-04-11 19:54:20 -0600

    shuffle butter

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,8 +3,8 @@ import './Navbar.css';
 
 const Navbar = props =>
     <nav className="navbar fixed-top navbar-light">
-        <h2 id="title">Clicky Game</h2>
-        <h2 id="guess">You guessed <span>correctly!</span></h2>
+        <h2 className="title">Clicky Game</h2>
+        <h2 className="title">South Park Edition</h2>
         <h2 id="score">Score: <span>{props.score}</span> | Top score: <span>{props.hiScore}</span></h2>
     </nav>
     ;
